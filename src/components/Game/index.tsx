65f6f2e1451c9feaa818c9cb851bdaec370608c2
aoshien <aoshisen@fanxiapp.com>
2023-06-28 17:12:20 +0800

    feat: 优化代码

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,27 +1,25 @@
+import React from "react";
 import Buttons from "./Buttons";
 import Screen from "./Screen";
-import { forwardRef } from "react";
-
-type CompoundedComponent = React.ForwardRefExoticComponent<
-  React.RefAttributes<HTMLElement>
-> & {
-  Buttons: typeof Buttons;
-  Screen: typeof Screen;
-};
-
 interface IGame {
   children?: any;
 }
 
-const InternalGame: React.ForwardRefRenderFunction<
-  HTMLButtonElement | HTMLAnchorElement,
-  IGame
-> = (props, ref) => {
-  const { children } = props;
-  return <div ref={ref}>{children}</div>;
+const GameInternal: React.FC<IGame> = ({ children }) => {
+  return (
+    <div>
+      Game
+      {children}
+    </div>
+  );
 };
 
-const Game = forwardRef(InternalGame) as CompoundedComponent;
+type ButtonComponent = typeof GameInternal & {
+  Buttons: typeof Buttons;
+  Screen: typeof Screen;
+};
+
+const Game = GameInternal as ButtonComponent;
 
 Game.Buttons = Buttons;
 Game.Screen = Screen;
